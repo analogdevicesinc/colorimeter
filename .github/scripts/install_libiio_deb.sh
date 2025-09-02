2ed409f471363cde796d08d8393ea2db62fcd169
Nicu Siderias <nicu.siderias@analog.com>
2025-09-02 09:26:31 +0300

    Create build workflow
    Add package folder for Debian template
    Add build workflow
    Add verify and create Debian sh
    Build and install libiio as a deb package
    
    Signed-off-by: Nicu Siderias <nicu.siderias@analog.com>

---
+++ .github/scripts/install_libiio_deb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+libiio_branch="libiio-v0"
+
+sudo apt-get -y update
+sudo apt-get -y install git build-essential libxml2-dev bison flex libcdk5-dev cmake libaio-dev libusb-1.0-0-dev libserialport-dev libavahi-client-dev doxygen graphviz python3 python3-pip python3-setuptools
+
+git clone -b "$libiio_branch" https://github.com/analogdevicesinc/libiio.git
+cd libiio
+
+mkdir build && cd build
+sudo cmake ../ -DCPP_BINDINGS=ON -DPYTHON_BINDINGS=ON -DENABLE_PACKAGING=ON
+sudo make -j$(nproc)
+sudo make package
+
+sudo dpkg -i libiio-*.deb
